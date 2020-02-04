d88ba1532c4753e2166774749c94dc98cd1a67f7
Andrey Alfaro <andrey.alfaro@epicsoftwaredev.com>
2020-02-04 16:11:08 -0600

    new AdminDashboard.tsx component

---
+++ src/components/admin/AdminDashboard.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from "react";
+import Paper from "@material-ui/core/Paper";
+
+interface IAdminDashboardProps {
+}
+
+const AdminDashboard: FunctionComponent<IAdminDashboardProps> = () => <Paper>
+    Admin dashboard
+</Paper>;
+
+export default AdminDashboard
+
+
