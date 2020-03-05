a6141dae4123ef5a6d21131b2da8f4a62f54bc11
Andrey Alfaro <andreyalfaro@gmail.com>
2020-03-05 00:33:21 -0600

    Fixes the problem with the background image on mobile

--- src/components/HomePage/Vision.tsx
+++ src/components/HomePage/Vision.tsx
@@ -8,10 +8,10 @@ import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
-            height: 'calc(100vh - 42px)',
             background: 'url(./images/dog1.jpg) no-repeat center',
             backgroundSize: 'cover',
             [theme.breakpoints.up('md')]: {
+                height: 'calc(100vh - 42px)',
                 backgroundAttachment: 'fixed',
             },
         },
