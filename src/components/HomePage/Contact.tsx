df5d540cda09a2a5c10b335ff89655b974b0a03e
Andrey Alfaro <andreyalfaro@gmail.com>
2020-03-06 23:26:58 -0600

    updated the landing page to include a follow us section, improved the social networks and responsive

--- src/components/HomePage/Contact.tsx
+++ src/components/HomePage/Contact.tsx
@@ -1,6 +1,11 @@
-import React, {ChangeEvent, FormEvent, FunctionComponent, useState} from "react";
+import React, {
+  ChangeEvent,
+  FormEvent,
+  FunctionComponent,
+  useState
+} from "react";
 import { useDispatch } from "react-redux";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
@@ -8,154 +13,161 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
-import RotateLeftIcon from '@material-ui/icons/RotateLeft';
-import SendIcon from '@material-ui/icons/Send';
-import PersonIcon from '@material-ui/icons/Person';
-import EmailIcon from '@material-ui/icons/Email';
-import MessageIcon from '@material-ui/icons/Message';
+import RotateLeftIcon from "@material-ui/icons/RotateLeft";
+import SendIcon from "@material-ui/icons/Send";
+import PersonIcon from "@material-ui/icons/Person";
+import EmailIcon from "@material-ui/icons/Email";
+import MessageIcon from "@material-ui/icons/Message";
 
 import useValidation from "../../constants/validations";
-import {IContact, IContactFactory, IContactSchema} from "../../store/emails/emails.types";
-import {SendContactEmail} from "../../store/emails/emails.actions";
+import {
+  IContact,
+  IContactFactory,
+  IContactSchema
+} from "../../store/emails/emails.types";
+import { SendContactEmail } from "../../store/emails/emails.actions";
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        form: {
-            margin: '40px 0',
-        },
-    }),
+  createStyles({
+    form: {
+      margin: "40px 0"
+    }
+  })
 );
 
 const Contact: FunctionComponent<{}> = () => {
-    const classes = useStyles();
-    const distpatch = useDispatch();
-    const {t} = useTranslation();
-    const [data, setData] = useState<IContact>(IContactFactory());
-    const {errors, setErrors, validate} = useValidation<IContact>(IContactSchema({
-        name: t('contact.name'),
-        email: t('contact.email'),
-        message: t('contact.message')
-    }), {abortEarly: true});
+  const classes = useStyles();
+  const distpatch = useDispatch();
+  const { t } = useTranslation();
+  const [data, setData] = useState<IContact>(IContactFactory());
+  const { errors, setErrors, validate } = useValidation<IContact>(
+    IContactSchema({
+      name: t("contact.name"),
+      email: t("contact.email"),
+      message: t("contact.message")
+    }),
+    { abortEarly: true }
+  );
 
-    function handleSubmit(event: FormEvent) {
-        event.preventDefault();
-        const valid = validate(data);
-        if(valid) {
-            console.log('send contact email');
-            distpatch(SendContactEmail(data));
-        }
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    const valid = validate(data);
+    if (valid) {
+      console.log("send contact email");
+      distpatch(SendContactEmail(data));
     }
+  }
 
-    function handleReset() {
-        setData(IContactFactory());
-        setErrors(null);
-    }
+  function handleReset() {
+    setData(IContactFactory());
+    setErrors(null);
+  }
 
-    function handleChange(field: keyof IContact) {
-        return (event: ChangeEvent<HTMLInputElement>) =>
-            setData({...data, [`${field}`]: event.target.value});
-    }
+  function handleChange(field: keyof IContact) {
+    return (event: ChangeEvent<HTMLInputElement>) =>
+      setData({ ...data, [`${field}`]: event.target.value });
+  }
 
-    return (<Container maxWidth="md">
-        <form
-            noValidate
-            className={classes.form}
-            onSubmit={handleSubmit}
-            onReset={handleReset}
-        >
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <Typography variant="h1">
-                        {t('contact.title')}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        label={t('contact.name')}
-                        placeholder={t('contact.name')}
-                        variant="outlined"
-                        fullWidth
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <PersonIcon/>
-                                </InputAdornment>
-                            ),
-                        }}
-                        value={data.name}
-                        error={!!errors?.name}
-                        helperText={errors?.name}
-                        onChange={handleChange('name')}
-                    />
-                </Grid>
-                <Grid item xs={12} md={6}>
-                    <TextField
-                        label={t('contact.email')}
-                        placeholder={t('contact.email')}
-                        variant="outlined"
-                        fullWidth
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <EmailIcon/>
-                                </InputAdornment>
-                            ),
-                        }}
-                        value={data.email}
-                        error={!!errors?.email}
-                        helperText={errors?.email}
-                        onChange={handleChange('email')}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        label={t('contact.message')}
-                        placeholder={t('contact.message')}
-                        variant="outlined"
-                        fullWidth
-                        multiline
-                        InputProps={{
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <MessageIcon/>
-                                </InputAdornment>
-                            ),
-                        }}
-                        value={data.message}
-                        error={!!errors?.message}
-                        helperText={errors?.message}
-                        onChange={handleChange('message')}
-                    />
-                </Grid>
-                <Grid container item spacing={2}>
-                    <Grid item xs={6}>
-                        <Button
-                            variant="contained"
-                            color="secondary"
-                            type="reset"
-                            fullWidth
-                            startIcon={<RotateLeftIcon/>}
-                        >
-                            {t('app.reset')}
-                        </Button>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            type="submit"
-                            fullWidth
-                            startIcon={<SendIcon/>}
-                        >
-                            {t('app.submit')}
-                        </Button>
-                    </Grid>
-                </Grid>
+  return (
+    <Container maxWidth="md">
+      <form
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+        onReset={handleReset}
+      >
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1">{t("contact.title")}</Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label={t("contact.name")}
+              placeholder={t("contact.name")}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PersonIcon />
+                  </InputAdornment>
+                )
+              }}
+              value={data.name}
+              error={!!errors?.name}
+              helperText={errors?.name}
+              onChange={handleChange("name")}
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label={t("contact.email")}
+              placeholder={t("contact.email")}
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <EmailIcon />
+                  </InputAdornment>
+                )
+              }}
+              value={data.email}
+              error={!!errors?.email}
+              helperText={errors?.email}
+              onChange={handleChange("email")}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              label={t("contact.message")}
+              placeholder={t("contact.message")}
+              variant="outlined"
+              fullWidth
+              multiline
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <MessageIcon />
+                  </InputAdornment>
+                )
+              }}
+              value={data.message}
+              error={!!errors?.message}
+              helperText={errors?.message}
+              onChange={handleChange("message")}
+            />
+          </Grid>
+          <Grid container item spacing={2}>
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="secondary"
+                type="reset"
+                fullWidth
+                startIcon={<RotateLeftIcon />}
+              >
+                {t("app.reset")}
+              </Button>
             </Grid>
-        </form>
-    </Container>);
+            <Grid item xs={6}>
+              <Button
+                variant="contained"
+                color="primary"
+                type="submit"
+                fullWidth
+                startIcon={<SendIcon />}
+              >
+                {t("app.submit")}
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 };
 
 export default Contact;
---
+++ src/components/HomePage/Social.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent } from "react";
+
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+import IconButton from '@material-ui/core/IconButton';
+
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+interface ISocialProps {
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+}
+
+const Social: FunctionComponent<ISocialProps> = (props) => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={4}>
+        <Button variant="contained" size="medium" startIcon={<FacebookIcon />}>
+          Facebook
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Button variant="contained" size="medium" startIcon={<TwitterIcon />}>
+          Twitter
+        </Button>
+      </Grid>
+      <Grid item xs={12} sm={4}>
+        <Button variant="contained" size="medium" startIcon={<InstagramIcon />}>
+          Instagram
+        </Button>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Social;
