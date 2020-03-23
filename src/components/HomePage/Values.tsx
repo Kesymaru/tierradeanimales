a273d6450dae4e0b66b4aa090c024ba327ce6592
Andrey Alfaro <andreyalfaro@Andreys-MacBook-Pro.local>
2020-03-22 20:36:27 -0600

    added background color to the calues component

--- src/components/HomePage/Values.tsx
+++ src/components/HomePage/Values.tsx
@@ -20,11 +20,13 @@ const useStyles = makeStyles((theme: Theme) =>
       flexDirection: "column",
       alignItems: "center",
       paddingTop: 40,
-      paddingBottom: 40
+      paddingBottom: 40,
+      backgroundColor: "#EDC45B"
+      // backgroundColor: "rgba(237, 196, 91, 0.5)",
     },
     card: {
       width: "100%",
-      backgroundColor: red[500]
+      backgroundColor: blue[500]
     },
     avatar: {
       backgroundColor: red[500]
@@ -44,7 +46,7 @@ const Values: FunctionComponent<{}> = () => {
         {Object.keys(values)
           .filter(key => typeof values[key] === "object")
           .map(key => (
-            <Card key={key} className={classes.card}>
+            <Card key={key} color="primary" className={classes.card}>
               <CardHeader
                 avatar={
                   <Avatar aria-label="recipe" className={classes.avatar}>
