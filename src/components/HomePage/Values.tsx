8719b5b74cae99076763a921b372dd0c3c28acac
Andrey Alfaro <andreyalfaro@gmail.com>
2020-03-31 00:23:49 -0600

    improved the values component responsive, hide the avatar icon

--- src/components/HomePage/Values.tsx
+++ src/components/HomePage/Values.tsx
@@ -5,6 +5,8 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { red, blue, green } from "@material-ui/core/colors";
+import useTheme from "@material-ui/core/styles/useTheme";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
@@ -26,11 +28,11 @@ const useStyles = makeStyles((theme: Theme) =>
       // backgroundColor: "rgba(237, 196, 91, 0.5)",
     },
     card: {
-      width: "100%",
-      backgroundColor: blue[500]
+      width: "100%"
+      // backgroundColor: blue[500]
     },
     avatar: {
-      backgroundColor: red[500]
+      // backgroundColor: red[500]
     },
     text: {
       display: "flex",
@@ -47,8 +49,10 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 const Values: FunctionComponent<{}> = () => {
+  const theme = useTheme();
   const classes = useStyles();
   const { t } = useTranslation();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const values = t("home.values", { returnObjects: true }) as any;
 
@@ -61,12 +65,14 @@ const Values: FunctionComponent<{}> = () => {
         {Object.keys(values)
           .filter(key => typeof values[key] === "object")
           .map(key => (
-            <Card key={key} color="primary" className={classes.card}>
+            <Card key={key} className={classes.card}>
               <CardHeader
                 avatar={
-                  <Avatar aria-label="recipe" className={classes.avatar}>
-                    {values[key].avatar}
-                  </Avatar>
+                  isMobile ? null : (
+                    <Avatar aria-label="recipe" className={classes.avatar}>
+                      {values[key].avatar}
+                    </Avatar>
+                  )
                 }
                 title={
                   <Typography variant="h4">{values[key].title}</Typography>
