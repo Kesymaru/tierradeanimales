1f20eae20bc3184a16f76c687be9eb1e359521ef
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-04 13:24:41 -0600

    added _results to the validations

--- src/constants/validations.ts
+++ src/constants/validations.ts
@@ -1,65 +1,83 @@
-import {useState} from "react";
-import {useTranslation} from "react-i18next";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-import {AnySchema, ValidationError, ValidationErrorItem, ValidationOptions, ValidationResult} from "@hapi/joi";
+import {
+  AnySchema,
+  ValidationError,
+  ValidationErrorItem,
+  ValidationOptions,
+  ValidationResult,
+} from "@hapi/joi";
 
 // ------------------------------------
 // Validation Hooks & Functions
 // ------------------------------------
-const DefaultOptions: ValidationOptions = {abortEarly: false, stripUnknown: true};
+const DefaultOptions: ValidationOptions = {
+  abortEarly: false,
+  stripUnknown: true,
+};
 
 export interface IValidationError {
-    [key: string]: string;
+  _results: any;
+  [key: string]: string;
 }
 
 export interface IUseValidation<T> {
-    errors: IValidationError | null;
-    setErrors: (error: IValidationError | null) => void;
-    validate: (value: T) => boolean;
+  errors: IValidationError | null;
+  setErrors: (error: IValidationError | null) => void;
+  validate: (value: T) => boolean;
 }
 
 function useValidation<T>(
-    schema: AnySchema,
-    options: ValidationOptions = DefaultOptions): IUseValidation<T> {
-    const {t} = useTranslation();
-    const [errors, setErrors] = useState<IValidationError | null>(null);
+  schema: AnySchema,
+  options: ValidationOptions = DefaultOptions
+): IUseValidation<T> {
+  const { t } = useTranslation();
+  const [errors, setErrors] = useState<IValidationError | null>(null);
 
-    const appMessages = t('app.errors', {returnObjects: true}) as object;
-    const messages = {...appMessages, ...options.messages} as Record<string, string>;
-    options = {
-        ...DefaultOptions,
-        ...options,
-        messages,
-    };
+  const appMessages = t("app.errors", { returnObjects: true }) as object;
+  const messages = { ...appMessages, ...options.messages } as Record<
+    string,
+    string
+  >;
+  options = {
+    ...DefaultOptions,
+    ...options,
+    messages,
+  };
 
-    function validate(value: T): boolean {
-        const results = Validator(schema, value, options);
-        const errors = getErrors(results);
-        setErrors(errors);
-        return !errors;
-    }
+  function validate(value: T): boolean {
+    const errors = getErrors(Validator(schema, value, options));
+    setErrors(errors);
+    return !errors;
+  }
 
-    return {errors, setErrors, validate};
+  return { errors, setErrors, validate };
 }
 
 function Validator<T>(
-    schema: AnySchema,
-    value: T,
-    options: ValidationOptions = DefaultOptions): ValidationResult {
-    options = {...DefaultOptions, ...options};
-    return schema.validate(value, options);
+  schema: AnySchema,
+  value: T,
+  options: ValidationOptions = DefaultOptions
+): ValidationResult {
+  options = { ...DefaultOptions, ...options };
+  return schema.validate(value, options);
 }
 
 function getErrors(results: ValidationResult): IValidationError | null {
-    const errors = results.error?.details.reduce((total, detail) =>
-        detail?.path?.length && detail?.message
-            ? ({
-                ...total,
-                [`${detail.path.join('.')}`]: detail.message
-            })
-            : total, {}) as IValidationError;
+  const errors = results.error?.details.reduce(
+    (total, detail) =>
+      detail?.path?.length && detail?.message
+        ? {
+            ...total,
+            [`${detail.path.join(".")}`]: detail.message,
+          }
+        : total,
+    {}
+  ) as IValidationError;
+  errors._results = results;
 
-    return !!errors ? errors : null;
+  return !!errors ? errors : null;
 }
 
 export default useValidation;
