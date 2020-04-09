43238232d994389d57274d15566963f370fbd88f
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-08 19:47:40 -0600

    updated user routes

--- src/User/routes/index.ts
+++ src/User/routes/index.ts
@@ -1,21 +1,21 @@
 import AccountBoxIcon from "@/user/routes/node_modules/@material-ui/icons/AccountBox";
 
-import { Route, RouteFactory } from "@/user/routes/node_modules/@app/core";
-import { DASHBOARD_ROUTE } from "@/user/routes/node_modules/@app/Dashboard/routes";
+import Route from "@core/models/route";
+import { DASHBOARD_ROUTE } from "@app/dashboard/routes";
 
 import Account from "../components/Account";
 
 // ------------------------------------
 // Account
 // ------------------------------------
-export const ACCOUNT_ROUTE: Route = RouteFactory({
+export const ACCOUNT_ROUTE: Route = {
   name: "Account",
   path: "/account",
   auth: true,
   component: Account,
   icon: AccountBoxIcon,
   parent: DASHBOARD_ROUTE,
-});
+};
 
 export const ROUTES: Route[] = [ACCOUNT_ROUTE];
 
