d0f48f96c85a3fa93555ca6579ee5793197a4d32
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-08 19:42:49 -0600

    updated foster home routes

--- src/foster-home/routes/index.ts
+++ src/foster-home/routes/index.ts
@@ -1,21 +1,20 @@
 import HouseIcon from "@material-ui/icons/House";
 import EditIcon from "@material-ui/icons/Edit";
 
-import Route from "@/core/components/node_modules/@core/models/route";
-import { RouteFactory } from "@/core/routes/node_modules/@core/hooks/route";
+import Route from "@core/models/route";
 import { ADMIN_DASHBOARD_ROUTE } from "@/dashboard/routes";
 import { FosterHomes, EditFosterHome } from "@/foster-home/components";
 
-export const FOSTER_HOMES_ROUTE: Route = RouteFactory({
+export const FOSTER_HOMES_ROUTE: Route = {
   name: "Foster Homes",
   path: "/homes",
   auth: true,
   component: FosterHomes,
   parent: ADMIN_DASHBOARD_ROUTE,
   icon: HouseIcon,
-});
+};
 
-export const EDIT_FOSTER_HOME_ROUTE: Route = RouteFactory({
+export const EDIT_FOSTER_HOME_ROUTE: Route = {
   name: "Edit Foster Home",
   path: `${FOSTER_HOMES_ROUTE.path}/:id`,
   defaultParams: { id: "new" },
@@ -23,7 +22,7 @@ export const EDIT_FOSTER_HOME_ROUTE: Route = RouteFactory({
   component: EditFosterHome,
   parent: FOSTER_HOMES_ROUTE,
   icon: EditIcon,
-});
+};
 
 const ROUTES: Route[] = [EDIT_FOSTER_HOME_ROUTE, FOSTER_HOMES_ROUTE];
 
