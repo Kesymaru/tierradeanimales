ba681e3af1f36089d73aab2bca8b0edb4f491973
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-11 02:11:12 -0600

    added adopt module files

---
+++ src/adopt/screens/Adopt.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+import Container from "@material-ui/core/Container";
+
+export const Adopt: FunctionComponent<{}> = () => {
+  return <Container>Here goes the adopt screen</Container>;
+};
+
+export default Adopt;
