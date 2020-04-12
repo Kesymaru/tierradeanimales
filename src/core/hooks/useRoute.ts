c153aad7f7fbabb8a319408d7af8bf3930bcb482
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-12 14:09:50 -0600

    useRoute hooks uses the ROUTES by default if not provided

--- src/core/hooks/useRoute.ts
+++ src/core/hooks/useRoute.ts
@@ -1,9 +1,10 @@
 import { useLocation } from "react-router-dom";
 
+import ROUTES from "../routes";
 import Route from "../models/route";
 import findRoute from "../utils/findRoute";
 
-export function useRoute(routes: Array<Route>): Route | undefined {
+export function useRoute(routes: Array<Route> = ROUTES): Route | undefined {
   const location = useLocation();
   return findRoute(routes, location);
 }
