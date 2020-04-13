c1f522ddede5f95a6b705704c028d9cb6f55e7ba
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-13 02:28:07 -0600

    removed not used import on app alert component

--- src/core/components/AppAlert.tsx
+++ src/core/components/AppAlert.tsx
@@ -4,7 +4,6 @@ import React, {
   FunctionComponent,
   SyntheticEvent,
 } from "react";
-import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert, { AlertProps as MuiAlertProps } from "@material-ui/lab/Alert";
 
