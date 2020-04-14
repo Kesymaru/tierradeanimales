86f95020acc6780d89cf23cfc682e8abd4f2510e
Andrey Alfaro <andreyalfaro@gmail.com>
2020-04-13 21:40:43 -0600

    Updated admin cases

--- src/Case/components/AdminCases.tsx
+++ src/Case/components/AdminCases.tsx
@@ -1,192 +1,104 @@
 import React, {
-  ChangeEvent,
+  FunctionComponent,
   useEffect,
   useState,
-  FunctionComponent,
+  ChangeEvent,
 } from "react";
-import { connect, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useFirestoreConnect, isLoaded, isEmpty } from "react-redux-firebase";
 import { useHistory } from "react-router-dom";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
+import { useTranslation } from "react-i18next";
+import get from "lodash/get";
 
-import Fade from "@material-ui/core/Fade";
-import Paper from "@material-ui/core/Paper";
-import Toolbar from "@material-ui/core/Toolbar";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import DeleteIcon from "@material-ui/icons/Delete";
+import Container from "@material-ui/core/Container";
+import Button from "@material-ui/core/Button";
+
 import AddIcon from "@material-ui/icons/Add";
-import EditIcon from "@material-ui/icons/Edit";
-import FilterListIcon from "@material-ui/icons/FilterList";
 
-import { Case } from "@app/case/models";
-import { ADMIN_CASE_EDIT_ROUTE } from "@app/case/routes";
+import { AppState } from "@core/models";
+import { CollectionsConfig } from "@core/config";
+import { AppTitle, AppTable, AppLoading, AppInfo } from "@core/components";
+
+import { ADMIN_EDIT_CASE_ROUTE } from "../routes";
+import Case from "../models";
+
+const { case: COLLECTION } = CollectionsConfig;
+const COLUMNS = [
+  {
+    title: "Name",
+    path: "name",
+  },
+  {
+    title: "Type",
+    path: "type",
+  },
+  {
+    title: "Sex",
+    path: "sex",
+  },
+  {
+    title: "Status",
+    path: "status",
+  },
+
+  {
+    title: "Public",
+    path: "public",
+  },
+];
 
 export const AdminCases: FunctionComponent<{}> = () => {
-  return <Paper>Here goes the new Admin Cases component</Paper>;
-
-  /* const dispatch = useDispatch();
   const history = useHistory();
-  const theme = useTheme();
-  const isSm = useMediaQuery(theme.breakpoints.down("sm"));
+  const { t } = useTranslation();
+  useFirestoreConnect({
+    collection: COLLECTION,
+  });
+  const data = useSelector<AppState, Array<Case>>((state) =>
+    get(state, `firestore.ordered.${COLLECTION}`)
+  );
 
-  const [dogs, _setDogs] = useState<Case[]>([]);
-  const [selected, setSelected] = useState<Case[]>([]);
-  const [loading, setLoading] = useState<boolean>(getLoading());
-  const [openDelete, setOpenDelete] = useState<boolean>(false);
-
-  // TODO
-  // user real data here
-  const dogs: Case[] = [];
-  const loading: boolean = false;
-
-  function getLoading(): boolean {
-    // return props.dogs.status === TStatus.Fetching;
-  }
-
-  function setDogs(value: IDog[]) {
-    setSelected(value.filter((dog) => dog._selected));
-    _setDogs(value);
-  }
-
-  function onChangePage(event: unknown, page: number) {
-    if (!props.dogs.pagination || page === props.dogs.pagination.page) return;
-    const pagination = { ...props.dogs.pagination, page };
-    dispatch(GetDogs(pagination));
-  }
-
-  function onChangeRowsPerPage(event: ChangeEvent<HTMLInputElement>) {
-    const rowPerPage = parseInt(event.target.value);
-    if (
-      !props.dogs.pagination ||
-      rowPerPage === props.dogs.pagination.rowPerPage
-    )
-      return;
-    const pagination = { ...props.dogs.pagination, rowPerPage };
-    dispatch(GetDogs(pagination));
-  }
-
-  function onSort(sort: ISort) {
-    console.log("on sort", sort);
-    dispatch(GetDogs(props.dogs.pagination, sort));
-  }
-
-  function handleDelete() {
-    // dispatch(DeleteDogs(selected))
-    // setOpenDelete(true);
+  if (!isLoaded(data)) return <AppLoading loading={true} />;
+  if (isLoaded(data) && isEmpty(data)) {
+    return (
+      <AppInfo
+        title={t("cases.errors.empty")}
+        message={t("cases.errors.emptyMessage")}
+        color="warning"
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => history.push(ADMIN_EDIT_CASE_ROUTE.getPath())}
+          startIcon={<AddIcon />}
+        >
+          {t("case.add")}
+        </Button>
+      </AppInfo>
+    );
   }
 
   return (
-    <Paper>
+    <Container>
+      <AppTitle title="Casos" />
       <AppTable
-        data={dogs}
-        cells={["name", "age", "sex", "status"]}
-        loading={loading}
-        pagination={props.dogs.pagination}
-        onSelect={(selected) => setDogs(selected)}
-        onSort={onSort}
-        onChangePage={onChangePage}
-        onChangeRowsPerPage={onChangeRowsPerPage}
+        columns={COLUMNS}
+        data={data}
+        title="Data title"
+        onSelectItem={(item: Case, i: number) =>
+          history.push(ADMIN_EDIT_CASE_ROUTE.getPath({ id: item.id }))
+        }
       >
-        <Toolbar variant={isSm ? "dense" : "regular"}>
-          {selected.length ? (
-            <Typography variant="subtitle1" style={{ flex: "1 1 100%" }}>
-              {selected.length} dogs selected
-            </Typography>
-          ) : (
-            <Typography variant="h6" style={{ flex: "1 1 100%" }}>
-              Dogs
-            </Typography>
-          )}
-
-          <Fade in={selected.length === 1}>
-            <Tooltip title="Edit Dog">
-              <IconButton
-                onClick={() =>
-                  history.push(
-                    ADMIN_CASE_EDIT_ROUTE.getPath(
-                      dogs.filter((i) => i._selected)[0]
-                    )
-                  )
-                }
-              >
-                <EditIcon />
-              </IconButton>
-            </Tooltip>
-          </Fade>
-          <Fade in={!!selected.length}>
-            <Tooltip title={`Delete Dog${selected.length > 1 ? "s" : ""}`}>
-              <IconButton onClick={handleDelete}>
-                <DeleteIcon />
-              </IconButton>
-            </Tooltip>
-          </Fade>
-          <Fade in={true}>
-            <Tooltip title="Add Dog">
-              <IconButton
-                onClick={() => history.push(ADMIN_CASE_EDIT_ROUTE.getPath())}
-              >
-                <AddIcon />
-              </IconButton>
-            </Tooltip>
-          </Fade>
-          {<Fade in={true}>
-                    <Tooltip title="Filter">
-                        <IconButton>
-                            <FilterListIcon/>
-                        </IconButton>
-                    </Tooltip>
-                </Fade>}
-        </Toolbar>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => history.push(ADMIN_EDIT_CASE_ROUTE.getPath())}
+          startIcon={<AddIcon />}
+        >
+          {t("case.add")}
+        </Button>
       </AppTable>
-
-      <AlertDialog
-        title={selected.length > 1 ? "Delete Dogs" : "Delete Dog"}
-        open={openDelete}
-        okTitle="Delete"
-        okColor="secondary"
-        okIcon={<DeleteIcon />}
-        cancelColor="primary"
-        onClose={() => setOpenDelete(false)}
-      >
-        {selected.length > 1 ? (
-          <>
-            <Typography variant="subtitle1">
-              This action will delete {selected.length} dogs permanently.
-            </Typography>
-            <ExpansionPanel>
-              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                <Typography>Delete details</Typography>
-              </ExpansionPanelSummary>
-              <ExpansionPanelDetails style={{ padding: "0 8px" }}>
-                <List aria-label="delete details" dense={true}>
-                  {selected.map((dog, index) => (
-                    <ListItem key={index}>
-                      <ListItemText primary={dog.name} />
-                    </ListItem>
-                  ))}
-                </List>
-              </ExpansionPanelDetails>
-            </ExpansionPanel>
-          </>
-        ) : (
-          <Typography variant="subtitle1">
-            {selected[0]
-              ? `This action will delete ${selected[0].name} permanently`
-              : "Selected dog will be deleted permanently."}
-          </Typography>
-        )}
-      </AlertDialog>
-    </Paper>
-  ); */
+    </Container>
+  );
 };
 
 export default AdminCases;
